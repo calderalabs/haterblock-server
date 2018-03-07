c4b0a56645bf1f7f8bdbc1408e4244c866bddee6
Matteo Depalo <matteodepalo@gmail.com>
2018-03-07 16:03:47 +0100

    WIP

--- bin/ci/install_docker_compose.sh
+++ bin/ci/install_docker_compose.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+# curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+# chmod +x /usr/local/bin/docker-compose
