00767c0ef50aae6e35db95dcfc97d20cbfb718d8
Matteo Depalo <matteodepalo@gmail.com>
2018-03-07 15:23:21 +0100

    Add docker-compose

--- bin/ci/build.sh
+++ bin/ci/build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
+docker-compose up -d
+
 mix local.hex --force
 mix local.rebar --force
 mix do deps.get, deps.compile
-
-apt-get update
-apt-get install postgresql-10.3
-createuser postgres
-psql -c "ALTER USER postgres PASSWORD '';"
-service postgresql restart
