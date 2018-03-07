53ae6d4dca1144382e82ec43cd77436b09b0fa9f
Matteo Depalo <matteodepalo@gmail.com>
2018-03-07 16:26:28 +0100

    Last try

--- bin/ci/install_docker_compose.sh
+++ bin/ci/install_docker_compose.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-/usr/local/bin/docker-compose up -d
+curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o ./docker-compose
+chmod +x ./docker-compose
+./docker-compose up -d
