fff74b31d5758d64aada3ea127ff21e87dacd950
Matteo Depalo <matteodepalo@gmail.com>
2018-03-07 16:28:46 +0100

    LUL

--- bin/ci/install_docker_compose.sh
+++ bin/ci/install_docker_compose.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o ./docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` > ./docker-compose
 chmod +x ./docker-compose
 ./docker-compose up -d
