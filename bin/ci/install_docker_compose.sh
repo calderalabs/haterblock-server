a4aaf278738ee895d3a5ee8bf8fb7bb993d693fa
Matteo Depalo <matteodepalo@gmail.com>
2018-03-07 16:09:45 +0100

    Fix script

--- bin/ci/install_docker_compose.sh
+++ bin/ci/install_docker_compose.sh
@@ -2,4 +2,4 @@
 
 curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-./usr/local/bin/docker-compose up -d
+/usr/local/bin/docker-compose up -d
