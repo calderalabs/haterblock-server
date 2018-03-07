8158cbfa5002edb9749fc10f25696b01d67da338
Matteo Depalo <matteodepalo@gmail.com>
2018-03-07 16:00:44 +0100

    Update install script

--- bin/ci/install_docker_compose.sh
+++ bin/ci/install_docker_compose.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-echo $PATH
