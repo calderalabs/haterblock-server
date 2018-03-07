f400d3471bd63311e86cb661b1a8b368b6685ea0
Matteo Depalo <matteodepalo@gmail.com>
2018-03-07 14:37:04 +0100

    Add postgres to the build script

--- bin/ci/build.sh
+++ bin/ci/build.sh
@@ -2,3 +2,9 @@
 mix local.hex --force
 mix local.rebar --force
 mix do deps.get, deps.compile
+
+apt-get update
+apt-get install postgresql-10.3
+createuser postgres
+psql -c "ALTER USER postgres PASSWORD '';"
+service postgresql restart
