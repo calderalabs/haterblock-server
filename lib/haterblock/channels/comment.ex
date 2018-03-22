5dcf2459c36c365ba2ca3b03ae91ce01e54abc5b
Matteo Depalo <matteodepalo@gmail.com>
2018-03-22 14:27:29 +0100

    Fetch comments from uploads list

--- lib/haterblock/channels/comment.ex
+++ lib/haterblock/channels/comment.ex
@@ -3,10 +3,9 @@ defmodule Haterblock.Channels.Comment do
   import Ecto.Changeset
   alias Haterblock.Channels.Comment
 
-
   schema "comments" do
-    field :body, :string
-    field :google_id, :string
+    field(:body, :string)
+    field(:google_id, :string)
 
     timestamps()
   end
@@ -16,5 +15,12 @@ defmodule Haterblock.Channels.Comment do
     comment
     |> cast(attrs, [:body, :google_id])
     |> validate_required([:body, :google_id])
+  end
+
+  def from_youtube_comment(youtube_comment) do
+    %Comment{
+      google_id: youtube_comment.id,
+      body: youtube_comment.snippet.textDisplay
+    }
   end
 end
