cce008638479c4ec8f0f8953b7fde6ad39393867
Matteo Depalo <matteodepalo@gmail.com>
2018-03-28 18:36:42 +0200

    Remove cache_static_manifest

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,6 @@ use Mix.Config
 config :haterblock, HaterblockWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "api.gethaterblock.com", port: 443],
-  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   force_ssl: [hsts: true]
 
