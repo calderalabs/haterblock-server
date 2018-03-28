0fb1ffeab57e8541be723cfedf3af7db3a8dd39e
Matteo Depalo <matteodepalo@gmail.com>
2018-03-28 17:08:20 +0200

    Whitelist names

--- lib/haterblock_web/controllers/auth_controller.ex
+++ lib/haterblock_web/controllers/auth_controller.ex
@@ -6,6 +6,10 @@ defmodule HaterblockWeb.AuthController do
   plug(Ueberauth)
   action_fallback(HaterblockWeb.FallbackController)
 
+  @allowed_names ["nemesys0", "eugeniodepalo", "MatchaLatteVlog"]
+
+  plug(:authorize_name)
+
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
     with {:ok, %User{} = user} <-
            Accounts.update_or_create_by_google_id(auth.uid, %{
@@ -18,6 +22,15 @@ defmodule HaterblockWeb.AuthController do
       |> put_status(:ok)
       |> put_resp_header("location", user_path(conn, :show))
       |> json(%{token: Haterblock.Auth.generate_token(%{sub: user.id}).token})
+    end
+  end
+
+  defp authorize_name(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
+    if !Enum.member?(@allowed_names, auth.info.name) do
+      conn
+      |> put_status(:unauthorized)
+      |> render(HaterblockWeb.ErrorView, :"401")
+      |> halt
     end
   end
 end
