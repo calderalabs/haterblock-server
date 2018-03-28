ee36eafdc37e7c0a641e343a946d29ba4bd1cec9
Matteo Depalo <matteodepalo@gmail.com>
2018-03-28 10:40:29 +0200

    Squash migrations

--- priv/repo/migrations/20180309173128_add_google_token_to_users.exs
+++
@@ -1,9 +0,0 @@
-defmodule Haterblock.Repo.Migrations.AddGoogleTokenToUsers do
-  use Ecto.Migration
-
-  def change do
-    alter table(:users) do
-      add(:google_token, :string)
-    end
-  end
-end
