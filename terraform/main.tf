217eea94e7f9f0718713b9cccbd2f8017844223e
Matteo Depalo <matteodepalo@gmail.com>
2018-03-28 18:03:26 +0200

    Config terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,27 @@
 provider "aws" {
-  region = "${var.aws_region}"
+  region     = "${var.aws_region}"
   access_key = "${var.aws_access_key_id}"
   secret_key = "${var.aws_secret_key}"
 }
 
+provider "dnsimple" {
+  token   = "${var.dnsimple_token}"
+  account = "${var.dnsimple_account}"
+}
+
 resource "aws_security_group" "web" {
   ingress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks  = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -26,18 +31,18 @@ resource "aws_key_pair" "web" {
 
 data "aws_ami" "web" {
   most_recent = true
-  owners = ["self"]
+  owners      = ["self"]
 
   filter {
-    name = "tag:Name"
+    name   = "tag:Name"
     values = ["${var.app_name}"]
   }
 }
 
 resource "aws_instance" "web" {
-  ami = "${data.aws_ami.web.id}"
-  instance_type = "${var.aws_instance_type}"
-  key_name = "${aws_key_pair.web.key_name}"
+  ami             = "${data.aws_ami.web.id}"
+  instance_type   = "${var.aws_instance_type}"
+  key_name        = "${aws_key_pair.web.key_name}"
   security_groups = ["${aws_security_group.web.name}"]
 
   lifecycle {
@@ -47,6 +52,13 @@ resource "aws_instance" "web" {
 
 resource "aws_eip" "web" {
   instance = "${aws_instance.web.id}"
+}
+
+resource "dnsimple_record" "a" {
+  domain = "${var.domain}"
+  name   = "api"
+  value  = "${aws_eip.web.public_ip}"
+  type   = "A"
 }
 
 output "public_ip" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,7 +2,16 @@ variable "ssh_public_key_file" {}
 variable "aws_access_key_id" {}
 variable "aws_secret_key" {}
 variable "aws_region" {}
-variable "app_name" {}
+variable "dnsimple_account" {}
+variable "dnsimple_token" {}
+
+variable "app_name" {
+  default = "haterblock"
+}
+
+variable "domain" {
+  default = "gethaterblock.com"
+}
 
 variable "aws_instance_type" {
   default = "t2.micro"
