72573697eed3a2e308c744979fa713d2979dc991
Matteo Depalo <matteodepalo@gmail.com>
2018-03-28 18:29:16 +0200

    Force ssl

--- config/prod.exs
+++ config/prod.exs
@@ -15,9 +15,10 @@ use Mix.Config
 # which you typically run after static files are built.
 config :haterblock, HaterblockWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "gethaterblock.com", port: 80],
+  url: [host: "api.gethaterblock.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  force_ssl: [hsts: true]
 
 # Do not print debug messages in production
 config :logger, level: :info
