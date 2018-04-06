1fdf7e753b1787631149b72fe5059989eeddc4b2
Matteo Depalo <matteodepalo@gmail.com>
2018-04-06 17:13:45 +0200

    Add db config

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "eugeniodepalo-terraform"
+    bucket = "calderalabs-terraform"
     region = "eu-west-1"
-    key = "haterblock"
+    key    = "haterblock"
   }
 }
