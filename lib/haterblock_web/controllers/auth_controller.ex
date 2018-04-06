d10e23269192f5ff70b9a90bc139ca5dae85aaf1
Matteo Depalo <matteodepalo@gmail.com>
2018-04-06 18:32:58 +0200

    Remove email whitelist

--- lib/haterblock_web/controllers/auth_controller.ex
+++ lib/haterblock_web/controllers/auth_controller.ex
@@ -6,15 +6,6 @@ defmodule HaterblockWeb.AuthController do
   plug(Ueberauth)
   action_fallback(HaterblockWeb.FallbackController)
 
-  @allowed_emails [
-    "serenamatchalatte@gmail.com",
-    "eugeniodepalo@gmail.com",
-    "nemesys0-1732@pages.plusgoogle.com",
-    "shantilives@gmail.com"
-  ]
-
-  plug(:authorize_email)
-
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
     with {:ok, %User{} = user} <-
            Accounts.update_or_create_by_google_id(auth.uid, %{
@@ -31,17 +22,6 @@ defmodule HaterblockWeb.AuthController do
       |> put_status(:ok)
       |> put_resp_header("location", user_path(conn, :show))
       |> json(%{token: Haterblock.Auth.generate_token(%{sub: user.id}).token})
-    end
-  end
-
-  defp authorize_email(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
-    if !Enum.member?(@allowed_emails, auth.info.email) do
-      conn
-      |> put_status(:unauthorized)
-      |> render(HaterblockWeb.ErrorView, :"401")
-      |> halt
-    else
-      conn
     end
   end
 end
