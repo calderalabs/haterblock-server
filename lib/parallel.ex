d8733252e69e38095ac8ba2050e79d657ce8a88f
Matteo Depalo <matteodepalo@gmail.com>
2018-05-02 11:08:08 +0200

    Increase pmap timeout

--- lib/parallel.ex
+++ lib/parallel.ex
@@ -2,6 +2,6 @@ defmodule Parallel do
   def pmap(collection, func) do
     collection
     |> Enum.map(&Task.async(fn -> func.(&1) end))
-    |> Enum.map(&Task.await/1)
+    |> Enum.map(&Task.await(&1, 30000))
   end
 end
